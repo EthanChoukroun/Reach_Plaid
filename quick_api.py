df9952241a67a796a4ea67159ae5fea699c97554
Ethan Choukroun <echoukroun@seatgeek.com>
2024-04-18 01:05:15 -0700

    add quick_api

---
+++ quick_api.py
@@ -0,0 +1,10 @@
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET'])
+def get_budget():
+    return jsonify({'smart_budget': 40})
+
+if __name__ == '__main__':
+    app.run(debug=True, port=5001)
