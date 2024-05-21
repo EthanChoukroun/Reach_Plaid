8483dbdcc714c7e464fc105d58c3197659241359
Ethan Choukroun <echoukroun@seatgeek.com>
2024-05-21 12:43:01 -0400

    More endpoints

--- quick_api.py
+++ quick_api.py
@@ -1,5 +1,4 @@
-from flask import Flask, jsonify
-
+from flask import Flask, jsonify, request
 
 app = Flask(__name__)
 
@@ -24,7 +23,23 @@ def expose_initial_status():
       'saving_predictions': 5250,
       'saving_rate_increase': 3
    })
-    
+
+@app.route('/referral_code', methods=['POST'])
+def referral_code():
+    try:
+      code = request.get_json()
+      if not code:
+          return jsonify({"error": "No code provided"}), 400
+      print(f"Received code: {code}")
+      return jsonify({"message": "Code received and printed to console"}), 200
+    except Exception as e:
+      print(f"Error: {e}")
+      return jsonify({"error": str(e)}), 500
+
+@app.route('/referral_code_confirmation', methods=["GET"])
+def referral_code_confirmation():
+   return jsonify({'confirmation': 1})
+
 if __name__ == '__main__':
   print("__main__")
   app.run(host='0.0.0.0', port=5002, debug=True)
